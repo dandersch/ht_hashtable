ec1f8694e36e823bc3b1fd77744f6b9e954d404c
da <>
2024-06-09 06:42:51 +0200

    add c89 support, remove <stdint.h> include

--- ht_hash_table.h
+++ ht_hash_table.h
@@ -1,7 +1,6 @@
 #pragma once
 
-#include <stddef.h>  // for size_t
-#include <stdint.h>  // for uint32_t, ...
+#include <stddef.h>  /* for size_t */
 
 struct ht_hash_table_t;
 typedef struct ht_hash_table_t ht_hash_table_t;
@@ -14,7 +13,7 @@ typedef struct ht_hash_table_t ht_hash_table_t;
  * - naive hash function
  */
 
-ht_hash_table_t* ht_init(void* (*allocator)(size_t),  const uint32_t table_size, const uint32_t entry_size);
+ht_hash_table_t* ht_init(void* (*allocator)(size_t),  const size_t table_size, const size_t entry_size);
 int              ht_add_entry(ht_hash_table_t* table, const char* key, void* entry);
 void*            ht_get_entry(ht_hash_table_t* table, const char* key);
 // TODO int      ht_free_entry(ht_hash_table_t* table, const char* key);
@@ -22,7 +21,7 @@ void*            ht_get_entry(ht_hash_table_t* table, const char* key);
 
 /* implementation */
 #ifdef HT_HASH_TABLE_IMPLEMENTATION
-#include <string.h> // for strlen(), strcmp(), memcpy()
+#include <string.h> /* for strlen(), strcmp(), memcpy() */
 
 typedef struct ht_entry_t
 {
@@ -34,33 +33,34 @@ typedef struct ht_hash_table_t
 {
     ht_entry_t* entries;
     void*       (*allocator)(size_t);
-    uint32_t    entry_size;
-    uint32_t    table_size;
+    size_t      entry_size;
+    size_t      table_size;
 } ht_hash_table_t;
 
-ht_hash_table_t* ht_init(void* (*allocator)(size_t), const uint32_t table_size, const uint32_t entry_size)
+ht_hash_table_t* ht_init(void* (*allocator)(size_t), const size_t table_size, const size_t entry_size)
 {
     assert(!(table_size&(table_size-1))); // size must be power of 2
-    uint32_t bytesize   = sizeof(ht_entry_t) * table_size;
+    size_t bytesize        = sizeof(ht_entry_t) * table_size;
     ht_hash_table_t* table = (ht_hash_table_t*) allocator(sizeof(ht_hash_table_t));
-    table->allocator    = allocator;
-    table->table_size   = table_size;
-    table->entry_size   = entry_size;
-    table->entries      = (ht_entry_t*) allocator(bytesize);
+    table->allocator       = allocator;
+    table->table_size      = table_size;
+    table->entry_size      = entry_size;
+    table->entries         = (ht_entry_t*) allocator(bytesize);
     memset(table->entries, 0, bytesize);
     return table;
 }
 
-static uint32_t ht_hash_function(ht_hash_table_t* table, const char* key)
+static size_t hash_function(ht_hash_table_t* table, const char* key)
 { 
-    // TODO better hash function
-    uint32_t ht_idx = 0;
-    for (int i = 0; i < strlen(key); i++) // unsecure
+    /* TODO better hash function */
+    size_t ht_idx = 0;
+    int i = 0;
+    for (i = 0; i < strlen(key); i++) /* unsecure */
     {
         ht_idx += key[i];
     }
 
-    // mask off high bits so that the index is under the hash table size
+    /* mask off high bits so that the index is under the hash table size */
     ht_idx &= (table->table_size / table->entry_size) - 1;
 
     return ht_idx;
@@ -68,13 +68,14 @@ static uint32_t ht_hash_function(ht_hash_table_t* table, const char* key)
 
 int ht_add_entry(ht_hash_table_t* table, const char* key, void* entry)
 {
-    uint32_t hash = ht_hash_function(table, key);
+    size_t hash = hash_function(table, key);
     int wrapped_around = 0;
 
-    // collision handling w/ internal chaining
-    for (uint32_t idx = hash; idx < table->table_size; idx++)
+    /* collision handling w/ internal chaining */
+    size_t idx = hash;
+    for (idx = hash; idx < table->table_size; idx++)
     {
-        if (table->entries[idx].key == NULL) // no collision
+        if (table->entries[idx].key == NULL) /* no collision */
         {
             table->entries[idx].entry = (void*) table->allocator(table->entry_size);
             memcpy(table->entries[idx].entry, entry, table->entry_size);
@@ -82,10 +83,10 @@ int ht_add_entry(ht_hash_table_t* table, const char* key, void* entry)
             return 1;
         }
 
-        // array is completely filled
-        if (idx == hash && wrapped_around) { return 0; }
+        /* array is completely filled */
+        if (idx == hash && wrapped_around)
 
-        if (idx == (table->table_size - 1)) // handle wraparound
+        if (idx == (table->table_size - 1)) /* handle wraparound */
         {
             wrapped_around = 1;
             idx = 0;
@@ -97,7 +98,7 @@ int ht_add_entry(ht_hash_table_t* table, const char* key, void* entry)
 
 void* ht_get_entry(ht_hash_table_t* table, const char* key)
 {
-    uint32_t hash       = ht_hash_function(table, key);
+    size_t hash  = hash_function(table, key);
     int wrapped_around = 0;
 
     for (uint32_t idx = hash; idx < table->table_size; idx++)
